2e6771c12526128d348e7e919c6ec6f9fbeaccb6
Matheus Germano <silvaa.matheus112@gmail.com>
2025-03-14 21:52:21 -0300

    Adding .prettierrc

--- src/Divisao.ts
+++ src/Divisao.ts
@@ -1,11 +1,11 @@
-import Calculo from "./Calculo";
+import Calculo from './Calculo'
 
 export default class Divisao extends Calculo {
   public calcular(number1: number, number2: number): number {
     if (number2 === 0) {
-      throw new Error("Não é possível dividir por zero");
+      throw new Error('Não é possível dividir por zero')
     }
 
-    return number1 / number2;
+    return number1 / number2
   }
 }
